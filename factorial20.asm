d2b9fa22292c97e3a4e58e4a259e5c5898da4c65
saadatibk <164902473+saadatibk@users.noreply.github.com>
2024-04-03 16:44:54 -0500

    Update factorial20.asm

--- factorial20.asm
+++ factorial20.asm
@@ -2,20 +2,21 @@ section .text
     global _start
 
 _start:
-    mov rdi, 20         ; Set the input number to 20
+    mov rdi, 20          ; Set the input number to 20
     call factorial     
     call exit_program
 
 factorial:
-    ; Input: rdi (number whose factorial is to be found)
-    ; Output: rax (factorial of the input number)
+                         ; Input: rdi (number whose factorial is to be found)
+                         ; Output: rax (factorial of the input number)
     mov rax, 1           
     cmp rdi, 1           ; Check if input is 1
     jbe .factorial_done  ; If input is 1, jump to the end
+
     .factorial_loop:
         imul rax, rdi    
         dec rdi          
-        cmp rdi, 1       ; Check if current number is 1
+        cmp rdi, 1         ; Check if current number is 1
         jg .factorial_loop ; If current number > 1, repeat loop
     .factorial_done:
     ret
